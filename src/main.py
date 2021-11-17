152f8528558507547b6f9117d68cb551e24f0e2e
Matthew Moulton <matthew.moulton@gmail.com>
2021-11-17 07:30:14 -0500

    refactor steps to collect information

--- src/main.py
+++ src/main.py
@@ -4,6 +4,39 @@ import re
 import src.constants as c
 import sys
 
+def find_files(params):
+    results = []
+    for root, dirs, files in os.walk(params['source']):
+        for file in files:
+            results.append({
+                'source_path': os.path.join(root, file),
+                'file': file,
+                'valid': True,
+            })
+    return results
+
+def add_date(params, results):
+    regex = re.compile(r'[0-9]{4}-[0-9]{2}-[0-9]{2}')
+    source = params['source']
+    prefix_length = len(source) if source.endswith('/') else len(source) + 1
+
+    for result in results:
+        date_path = result['source_path'][prefix_length:];
+        match = regex.match(date_path.replace('/', '-'))
+        if match:
+            result['date'] = match.group()
+        else:
+            result['valid'] = False
+            result['reason'] = 'Invalid Date'
+
+def print_results(params, results):
+    print(params)
+    for result in results:
+        if (result['valid']):
+            print(result)
+        else:
+            print(f'{c.color.ERROR}{result}{c.color.CLEAR}')
+
 @click.command()
 @click.argument('source')
 @click.argument('destination')
@@ -18,31 +51,19 @@ combination of directory or filename characters. For example,
 
 Affected images will be moved to DESTINATION/YYYY and will be renamed to include
 the YYYY-MM-DD date in the filename."""
-    print(f"src: {source}")
-    print(f"dst: {destination}")
-    print(f"v: {verbose}")
-    print(f"d: {dry_run}")
 
+    if not os.path.exists(source):
+        sys.exit('Source does not exist: ' + source)
+    if not os.path.exists(destination):
+        sys.exit('Destination does not exist: ' + destination)
 
-def old():
-    if len(sys.argv) < 2:
-        sys.exit("missing path argument")
+    params = {
+        'source': source,
+        'destination': destination,
+        'verbose': verbose,
+        'dry_run': dry_run,
+    }
 
-    root_path = sys.argv[1]
-
-    if not os.path.exists(root_path):
-        sys.exit("path does not exist: " + root_path)
-
-    regex = re.compile(r'[0-9]{4}-[0-9]{2}-[0-9]{2}')
-    prefix_length = len(root_path) if root_path.endswith("/") else len(root_path) + 1
-
-    def handle(path):
-        match = regex.match(path[prefix_length:].replace("/", "-"))
-        if match:
-            print(f"{path} {match.group()}")
-        else:
-            print(f"{c.color.ERROR}Error!{c.color.CLEAR} {path}")
-
-    for root, dirs, files in os.walk(root_path):
-        for file in files:
-            handle(os.path.join(root, file))
+    results = find_files(params)
+    add_date(params, results)
+    print_results(params, results)
